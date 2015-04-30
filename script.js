457a60bd0d4d42ec0590128215a0aa668b63c63d
Carlos Socorro <carlosrsocorro@gmail.com>
2015-04-30 16:57:30 -0400

    commiting complete task

---
+++ script.js
@@ -0,0 +1,32 @@
+var main = function(){
+
+$('.cart').click(function(){
+	$('.account .dropdown-menu').hide();
+	$('.help .dropdown-menu').hide();
+	$('.cart .dropdown-menu').toggle();
+});
+$('.account').click(function(){
+	$('.cart .dropdown-menu').hide();
+	$('.help .dropdown-menu').hide();
+	$('.account .dropdown-menu').toggle();
+}); 
+$('.help').click(function(){
+	$('.cart .dropdown-menu').hide();
+	$('.account .dropdown-menu').hide();
+	$('.help .dropdown-menu').toggle();
+});
+
+};
+
+$(document).ready(main);
+
+
+
+
+// // Slider Functionality
+
+$(function() {
+    $('.banner').unslider();
+});
+
+$(document).ready(main);
